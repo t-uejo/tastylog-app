3ca091a8c444f465e57af5e46a8d59305e2e98c5
t-uejo <tiki.ur.tiki@icloud.com>
2025-09-06 14:06:04 +0900

    インターネットゲートウェイ作成

--- network.tf
+++ network.tf
@@ -116,3 +116,20 @@ resource "aws_route_table_association" "private_rt_1c" {
   subnet_id      = aws_subnet.private_subnet_1c.id
 }
 
+# ---------------------------
+# Internet Gateway
+# ---------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.env}-igw"
+    Project = var.project
+    Env     = var.env
+  }
+}
+
+resource "aws_route" "public_rt_igw" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
