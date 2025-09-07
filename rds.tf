17bcd53d83cf080133983810eedb6f3542614c48
t-uejo <tiki.ur.tiki@icloud.com>
2025-09-07 17:06:58 +0900

    RDSのパラメータグループ作成

---
+++ rds.tf
@@ -0,0 +1,19 @@
+# ---------------------------
+# RDS
+# ---------------------------
+# RDS Instance
+resource "aws_db_parameter_group" "mysql_standalone_parameter_group" {
+  name        = "${var.project}-${var.env}-mysql-standalone-pg"
+  family      = "mysql8.0"
+  description = "MySQL Standalone Parameter Group"
+
+  parameter {
+    name  = "character_set_database"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+}
