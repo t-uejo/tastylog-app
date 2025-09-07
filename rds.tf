90c5f9d2b1db578882d2e933defaa32a348cb0da
t-uejo <tiki.ur.tiki@icloud.com>
2025-09-07 17:27:34 +0900

    RDSオプショングループ作成

--- rds.tf
+++ rds.tf
@@ -1,5 +1,5 @@
 # ---------------------------
-# RDS
+# RDS Parameter Group
 # ---------------------------
 # RDS Instance
 resource "aws_db_parameter_group" "mysql_standalone_parameter_group" {
@@ -16,4 +16,13 @@ resource "aws_db_parameter_group" "mysql_standalone_parameter_group" {
     name  = "character_set_server"
     value = "utf8mb4"
   }
+}
+
+# ---------------------------
+# RDS Option Group
+# ---------------------------
+resource "aws_db_option_group" "mysql_standalone_option_group" {
+  name                 = "${var.project}-${var.env}-mysql-standalone-og"
+  engine_name          = "mysql"
+  major_engine_version = "8.0"
 }
