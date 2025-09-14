8ff5e9409b1855f8aa35daefcec2cea152a136dc
t-uejo <tiki.ur.tiki@icloud.com>
2025-09-14 11:05:29 +0900

    Add EC2 instance resource for app server with necessary configurations and tags

--- app-server.tf
+++ app-server.tf
@@ -11,3 +11,27 @@ resource "aws_key_pair" "keypair" {
     Env     = var.env
   }
 }
+
+
+# ---------------------------
+# EC2 Instance
+# ---------------------------
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.keypair.key_name
+  security_groups = [
+    aws_security_group.app_sg.id,
+    aws_security_group.operation_sg.id
+  ]
+
+  tags = {
+    Name    = "${var.project}-${var.env}-app-ec2"
+    Project = var.project
+    Env     = var.env
+    Type    = "app"
+  }
+}
+
