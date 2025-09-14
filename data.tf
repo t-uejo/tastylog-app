35933cbd763deffba43fbbef2f5794581f1a68f1
t-uejo <tiki.ur.tiki@icloud.com>
2025-09-14 10:06:52 +0900

    AWS AMIのimageを取得

--- data.tf
+++ data.tf
@@ -1,3 +1,23 @@
 data "aws_prefix_list" "s3_pl" {
   name = "com.amazonaws.*.s3"
 }
+
+data "aws_ami" "app" {
+  most_recent = true
+  owners      = ["amazon", "self"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.8.*-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
