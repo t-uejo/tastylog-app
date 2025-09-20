f5e45dd439864200f7b635cc3b94c2e31fd2dc7c
t-uejo <tiki.ur.tiki@icloud.com>
2025-09-20 13:16:31 +0900

    Add SSM parameters for MySQL configuration in app server

--- app-server.tf
+++ app-server.tf
@@ -36,3 +36,35 @@ resource "aws_instance" "app_server" {
   }
 }
 
+# ---------------------------
+# SSM Parameter Store
+# ---------------------------
+resource "aws_ssm_parameter" "host" {
+  name  = "/${var.project}/${var.env}/app/MYSQL_HOST"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.address
+}
+
+resource "aws_ssm_parameter" "port" {
+  name  = "/${var.project}/${var.env}/app/MYSQL_PORT"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.port
+}
+
+resource "aws_ssm_parameter" "database" {
+  name  = "/${var.project}/${var.env}/app/MYSQL_DATABASE"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.db_name
+}
+
+resource "aws_ssm_parameter" "username" {
+  name  = "/${var.project}/${var.env}/app/MYSQL_USERNAME"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.username
+}
+
+resource "aws_ssm_parameter" "password" {
+  name  = "/${var.project}/${var.env}/app/MYSQL_PASSWORD"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.password
+}
